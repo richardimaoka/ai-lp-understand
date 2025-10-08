b1af7ad99465d1d6d7445d1b8307f5d4dca6c0f3
richardimaoka <richard.s.imaoka@gmail.com>
2025-10-09 07:52:38 +0900

    break line

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,10 +1,14 @@
-import styles from './HeroSection.module.css';
+import styles from "./HeroSection.module.css";
 
 export default function HeroSection() {
   return (
     <section className={`${styles.section} ${styles.heroSection}`}>
       <div className={styles.heroContent}>
-        <h1>革新的なソリューションでビジネスを加速</h1>
+        <h1>
+          革新的なソリューションで
+          <br />
+          ビジネスを加速
+        </h1>
         <p>
           あなたのビジネスの課題を解決し、成長をサポートする最先端のサービスを提供します。
         </p>
