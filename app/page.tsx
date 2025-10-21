3527cdb562975cb3bf2cb3dd8e4c104eaa9d29f6
richardimaoka <richard.s.imaoka@gmail.com>
2025-10-21 18:29:14 +0900

    clear

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,3 @@
-import styles from "./page.module.css";
-import HeroSection from "./components/HeroSection";
-import ServiceFeaturesSection from "./components/ServiceFeaturesSection";
-import TestimonialsSection from "./components/TestimonialsSection";
-import KeyFeaturesSection from "./components/KeyFeaturesSection";
-import PricingSection from "./components/PricingSection";
-import FooterSection from "./components/FooterSection";
-
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <HeroSection />
-      <ServiceFeaturesSection />
-      <TestimonialsSection />
-      <KeyFeaturesSection />
-      <PricingSection />
-      <FooterSection />
-    </div>
-  );
+  return <div>hello world</div>;
 }
